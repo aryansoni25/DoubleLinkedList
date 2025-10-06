a441919fc78aacbb062a774c7a367efb171f1523
aryansoni25 <ashoksoni9425@gmail.com>
2025-10-06 14:15:15 +0530

    1st Commit

--- src/main/java/org/studyeasy/Main.java
+++ src/main/java/org/studyeasy/Main.java
@@ -9,8 +9,8 @@ public class Main {
         dll.insertValue(11,2);
         dll.insertValue(12,3);
         dll.insertValue(13,4);
-//        System.out.println(dll.tail.value);
-//        System.out.println(dll.head.value);
+        System.out.println(dll.tail.value);
+        System.out.println(dll.head.value);
     dll.traverse();
 //        dll.reverse();
 //        dll.searchValue(7);
